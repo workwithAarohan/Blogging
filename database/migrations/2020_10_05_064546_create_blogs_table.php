704fdb37cacc7f7f34ff54fc82511bc1da9cdeb6
Aarohan Nakarmi <52736805+workwithAarohan@users.noreply.github.com>
2020-10-05 18:16:57 +0545

    Additional (#2)
    * .env file added
    
    * composer.lock added
    
    * file updated
    
    * Blog table created
    
    Co-authored-by: Raman04210 <ramantandukar2015@gmail.com>

---
+++ database/migrations/2020_10_05_064546_create_blogs_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('image');
+            $table->string('content');
+            $table->string('description');
+            $table->integer('cat_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            
+            $table->foreign('cat_id')->references('id')->on('categories')
+                ->onDelete('cascade')->onUpdate('cascade');
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
