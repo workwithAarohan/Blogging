33c7aae32f3f7bec7280bf8c2f3df138618275f3
workwithAarohan <aarohan.nakarmi.29@gmail.com>
2020-10-06 15:08:43 +0545

    Few changes in Category table

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
+@section('title')
+    Create New Category
+@endsection
+
 @section('content')
     <div class="container mt-5">
         <div class="row justify-content-center">
-            <div class="col col-md-5 border p-4">
+            <div class="col col-md-5 p-4 shadow bg-white rounded">
                 <h4>Add Category</h4><hr>
                 <form action="{{URL::to('category')}}" method="POST" class="form-group">
                     @csrf 
